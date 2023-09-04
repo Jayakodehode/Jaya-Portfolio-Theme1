d97e0c887849d32bb9a60c5ef6410557ee4081a3
Jayakodehode <jaya.kodehode@gmail.com>
2023-09-04 14:52:22 +0200

    added media queries for various screen sizes

--- js/script.js
+++ js/script.js
@@ -1,45 +1,11 @@
 const menu = document.querySelector("#menubar");
 const header = document.querySelector(".header");
+const i = document.getElementsByClassName("fa-solid");
 
 function onClick() {
+  menu.classList.toggle("fa-times");
   header.classList.toggle("active");
 }
 window.onscroll = () => {
   header.classList.remove("active");
 };
-
-/**  about page opentabs */
-
-const tabLinks = document.getElementsByClassName("tab-links");
-const tabContents = document.getElementsByClassName("tab-contents");
-const link = document.getElementsByClassName("link");
-function openTab(tabname) {
-  for (const tablink of tabLinks) {
-    if (tablink.classList.contains("active-link")) {
-      tablink.classList.remove("active-link");
-    }
-    tablink.classList.add("active-link");
-  }
-  for (const tabcontent of tabContents) {
-    tabcontent.classList.remove("active-tab");
-  }
-
-  document.getElementById(tabname).classList.add("active-tab");
-  tabname.classList.toggle("active-link");
-}
-
-/* ------ JS code  projecttab links  */
-
-const projecttabLinks = document.getElementsByClassName("projecttab-links");
-const projecttabContents = document.getElementsByClassName(
-  "projecttab-contents"
-);
-function openProjectTab(tabname) {
-  for (const tablink of projecttabLinks) {
-    tablink.classList.remove("projectactive-link");
-  }
-  for (const tabcontent of projecttabContents) {
-    tabcontent.classList.remove("projectactive-tab");
-  }
-  document.getElementById(tabname).classList.add("projectactive-tab");
-}
