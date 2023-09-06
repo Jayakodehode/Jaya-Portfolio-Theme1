05e366ec983efb67561db2c0ac5a413f18540864
Jayakodehode <jaya.kodehode@gmail.com>
2023-09-06 14:53:16 +0200

    updated color themes

--- js/stylewsither.js
+++ js/stylewsither.js
@@ -4,6 +4,7 @@ function changeTheme() {
   darkLight.querySelector("i").classList.toggle("fa-moon");
   darkLight.querySelector("i").classList.toggle("fa-sun");
   document.body.classList.toggle("light");
+  resetColors();
 }
 
 window.addEventListener("load", () => {
@@ -17,14 +18,31 @@ window.addEventListener("load", () => {
 
 document.querySelectorAll(".color-picker .color").forEach((color) => {
   color.addEventListener("click", () => {
-    console.log("color");
     const maincolor = color.getAttribute("data-color");
-    console.log(maincolor);
-    document
-      .querySelector(":root")
-      .style.setProperty("--main-color", maincolor);
-    document
-      .querySelector("body.light")
-      .style.setProperty("--main-color", maincolor);
+
+    if (document.body.classList.contains("light")) {
+      document
+        .querySelector("body.light")
+        .style.setProperty("--main-color", maincolor);
+    } else {
+      document.body.style.setProperty("--main-color", maincolor);
+    }
   });
 });
+
+function resetColors() {
+  const originalLightColor = "#680ea0"; // Replace with your original light mode color
+  const originalDarkColor = "#07edec"; // Replace with your original dark mode color
+
+  document
+    .querySelector(":root")
+    .style.setProperty("--main-color", originalDarkColor);
+
+  if (document.body.classList.contains("light")) {
+    document
+      .querySelector("body.light")
+      .style.setProperty("--main-color", originalLightColor);
+  } else {
+    document.body.style.setProperty("--main-color", originalDarkColor);
+  }
+}
